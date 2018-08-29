6d2d0db246f73c5d3f62e13da4b03147765ee130
bzanetis <britt.zanetis19@gmail.com>
2018-08-29 13:07:02 -0500

    Checkpoint 3 create chat rooms: completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -9,7 +9,8 @@ class RoomList extends Component {
        super(props);
 
        this.state = {
-       rooms: []
+       rooms: [],
+       newRoomName: ''
      };
 
      this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -17,19 +18,37 @@ class RoomList extends Component {
 
 
      componentDidMount(){
-                 this.roomsRef.on('child_added',snapshot =>{
-                     const room = snapshot.val();
-                     room.key= snapshot.key
-                     this.setState({ rooms: this.state.rooms.concat(room)});
-                 } )
-             }
+     this.roomsRef.on('child_added',snapshot =>{
+        const room = snapshot.val();
+        room.key= snapshot.key
+        this.setState({ rooms: this.state.rooms.concat(room)});
+      } )
+     }
 
+     createRoom(newRoomName){
+       this.roomsRef.push({
+         name:newRoomName
+       });
+       this.setState({newRoomName:''});
+     }
+
+     handleChange(e) {
+       this.setState({newRoomName: e.target.value });
+     }
 
 
 
    render() {
       return (
         <div className= "rooms">
+
+
+         <form onSubmit={e=>{e.preventDefault();this.createRoom(this.state.newRoomName)}}>
+          <input type = "text" placeholder="Add new room" value={this.state.newRoomName} onChange={(e)=> this.handleChange(e)}/>
+          <button className="adding_rooms_button">Add</button>
+         </form>
+
+
          <section className= "room-list">
           {this.state.rooms.map((room, index)=>
             <div className="room_names" key={index}>
